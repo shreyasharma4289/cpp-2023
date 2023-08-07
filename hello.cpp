74f27ec7f56e4e2f252eaac258dcf01f9e809ccc
Shreya Sharma <120007301+shreyasharma4289@users.noreply.github.com>
2023-08-07 22:05:37 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//	int myInteger=5;
+//	myInteger=10; //overwriting
+//	//const keyword declared before variable name to avoid override
+//	double myDouble=2.3;
+//	char myChar='a';
+//	bool myBool=true;
+//	string st="hello";
+//	cout << "hello world" <<endl;  
+//	cout << "hello all" <<endl;
+//	cout<<myInteer<<endl;
+//	cout<<y<<endl;
+//	cout<<ch<<endl;
+	
+//	string userInput;
+//	
+//	cout << "enter a name : ";
+//	cin>>userInput;
+//	cout<< "number is: "<< userInput <<endl;
+//	return 0;
+
+
+//2inputs +,-,*,/,%
+	int firstNum, secondNum;
+	cout<< "enter first number ";
+	cin >>firstNum ;
+	cout<<"enter second number ";
+	cin >> secondNum;
+	cout<< "sum is: "<< firstNum+secondNum<<endl;
+	cout<< "difference is: "<< firstNum-secondNum<<endl;
+	cout<< "product is: "<< firstNum*secondNum<<endl;
+	cout<< "quotient is: "<< firstNum/secondNum<<endl;
+	cout<< "mod is: "<< firstNum%secondNum<<endl;
+	
+	return 0;
+	
+	
+	
+}
