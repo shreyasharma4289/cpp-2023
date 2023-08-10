e9e133595c6e60407c00f6b86c08fc457777934e
Shreya Sharma <120007301+shreyasharma4289@users.noreply.github.com>
2023-08-10 20:18:19 +0530

    Add files via upload

---
+++ array1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+//Array; collection of similiar data element
+
+int main()
+{
+	int arr[5]={1,2,3,4.5};
+	cout<<arr[3]<<endl;;
+	arr[3]=8;
+	cout<< "after changing" <<arr[3];
+}
