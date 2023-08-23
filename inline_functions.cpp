dcb7a2241bcbe399827e4442c979be431f5cb33e
Shreya Sharma <120007301+shreyasharma4289@users.noreply.github.com>
2023-08-23 19:02:38 +0530

    Add files via upload

---
+++ inline_functions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+inline int add(int a,int b)
+{
+	return a+b;
+	
+}
+int main()
+{
+	int a=6;
+	int b=5;
+	cout<<"A+B: "<<int(a,b);
+	cout<<"A+B: "<<int(a,b);
+}
