15dc36aa914994711a9e5b52e36e111d5486e1f8
Shreya Sharma <120007301+shreyasharma4289@users.noreply.github.com>
2023-09-27 00:16:21 +0530

    Add files via upload

---
+++ stack_middleElement_deletion.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void deleteMiddle(stack<int> &inputStack, int count, int size)
+{
+	if(inputStack.empty() || count == size/2)
+	{
+		inputStack.pop();
+		return;
+	}
+		
+		int num= inputStack.top();
+		inputStack.pop();
+		
+		//recursive call
+		
+		deleteMiddle(inputStack, count+1, size);
+		
+		inputStack.push(num);
+	
+}
+	int main()
+	{
+		stack<int> s;
+		s.push(1);
+		s.push(2);
+		s.push(3);
+		s.push(4);
+		s.push(5);
+		
+		int size =s.size();
+		
+		cout<<"stack before delettion:";
+		
+		stack<int> tempStack=s;
+		
+		while(!tempStack.empty())
+		{
+			cout<< tempStack.top()<<" ";
+			tempStack.pop();		}
+			cout<<endl;
+			
+			deleteMiddle(s,0,size);
+			
+			cout<<"stack after DEletion: ";
+			
+			while(!s.empty())
+			{
+				cout<<s.top()<<" ";
+				s.pop();
+			}
+			cout<<endl;
+	}
